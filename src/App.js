df067109083f9e153f302392889e7936c1213188
wajiha kouser <wajihawajid5610@gmail.com>
2023-09-01 21:09:57 +0500

    counterApp using hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, {useState} from 'react';
 import './App.css';
-
+import Parent from './Parent';
 
 export default function App() {
+  return(
+    <div>
+      <Parent />
+    </div>
 
-  let [count, updateCounter] = useState(0);
+  ); 
+}
+
+
+/*let [count, updateCounter] = useState(0);
   let [day, changeDay] = useState(true);
   return (
-    
     <div className={`main ${day ? "light" : "dark"}`}>
-     
-     <h1 className='h1'>Counter App</h1><br/><br/>
-     
-     <h2 className='h2'>The value of counter is {count}</h2>
-     <br/><br/>
-     <button onClick={()=> updateCounter(count + 1)} className='btn'>Increase Counter</button>&nbsp;&nbsp;&nbsp;&nbsp;
+    <h1 className='h1'>Counter App</h1><br/><br/> 
+    <h2 className='h2'>The value of counter is {count}</h2>
+    <br/><br/>
+    <button onClick={()=> updateCounter(count + 1)} className='btn'>Increase Counter</button>&nbsp;&nbsp;&nbsp;&nbsp;
      <button onClick={()=> updateCounter(count - 1)} className='btn'>Decrease Counter</button> 
      <br/><br/>
      <button onClick={()=> changeDay(!day)} className='btn'>changeMode</button>
     </div>
-  );
-}
+  );*/
---
+++ src/Parent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Child from './Child';
+
+const Parent = () =>{
+
+    return(
+        <Child />
+
+    );
+}
+export default Parent;
