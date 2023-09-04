eb4525d1a2b1d0b8e0f7e93763f3ec166a5712ae
wajiha kouser <wajihawajid5610@gmail.com>
2023-09-04 12:36:58 +0500

    Update Parent.js

--- src/Parent.js
+++ src/Parent.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Child from './Child';
 
-const Parent = (props) =>{
+const Parent = () =>{
     return(
-        <Child name={props.name}/>
+        <Child/>
     );
 }
-export default Child;
+export default Parent;
