fe51e1dfb516730642a9bb758801300e1c528b62
wajiha kouser <wajihawajid5610@gmail.com>
2023-09-06 21:01:03 +0500

    updated

--- src/Child.js
+++ src/Child.js
@@ -5,7 +5,8 @@ import counterReducer from './counterReduser';
 
 
 const Child= () =>{
-let [count, updateCounter] = useContext(counterContext)
+ 
+let [count = 0, updateCounter] = useContext(counterContext)
 let [day, changeDay] = useState(true);
 
 let [state,dispatch]= useReducer(counterReducer,0);
@@ -16,7 +17,7 @@ let [state,dispatch]= useReducer(counterReducer,0);
 
      <h2 className='h2'>The value of Context counter is: {count}</h2>  
      <button onClick={()=> updateCounter(++count)} className='btn'>Increase Counter</button>&nbsp;&nbsp;&nbsp;&nbsp;
-     <button onClick={()=> updateCounter(--count)} className='btn'>Decrease Counter</button> 
+     <button onClick={()=> {if (count!==0)updateCounter(--count); else alert('counter not be less than zero');count = 0 }}className='btn'>Decrease Counter</button> 
      <br/><br/><br/><br/>
 
 
--- src/counterReduser.js
+++ src/counterReduser.js
@@ -1,9 +1,16 @@
-const counterReducer = (state, action) =>{
+const counterReducer = (state, action) => {
     switch (action){
         case 'Increment':
             return state + 1
         case 'Decrement':
-            return state -1
-    };
+            if (state !== 0)
+              return state -1 
+            else
+                alert('not be less than zero')
+                return state = 0
+                
+        default:
+             throw new Error();
+    }
 }
 export default counterReducer;
